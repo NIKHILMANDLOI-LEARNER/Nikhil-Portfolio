7e8ba1c657085e6a7e7696320933392b1fe22fc0
NIKHILMANDLOI-LEARNER <nikhil.25bcs10100@sst.scaler.com>
2025-10-05 21:54:57 +0530

    update

--- script.js
+++ script.js
@@ -3,4 +3,25 @@ const nav = document.querySelector('.nav');
 
 menuToggle.addEventListener('click', () => {
   nav.classList.toggle('active');
-});
+
+ });
+ document.getElementById('bb').onclick = function() {
+  document.getElementById('popupBox').style.display = 'flex';
+};
+document.getElementById('bp').onclick = function() {
+  document.getElementById('popupBo').style.display = 'flex';
+};
+
+document.getElementById('bk').onclick = function() {
+  document.getElementById('popup').style.display = 'flex';
+};
+document.getElementById('closePopup').onclick = function() {
+  document.getElementById('popupBox').style.display = 'none';
+};
+document.getElementById('closePopu').onclick = function() {
+  document.getElementById('popupBo').style.display = 'none';
+};
+
+document.getElementById('closePo').onclick = function() {
+  document.getElementById('popup').style.display = 'none';
+};
